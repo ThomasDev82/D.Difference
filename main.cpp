6d64b20b10e35e217f4ac511b94bdd5f56f15301
ThomasDev82 <lancevancy7@gmail.com>
2022-08-11 02:11:21 +0200

    Problem - D

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    long long a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+
+    long long res = (a*b)-(c*d);
+
+    cout << "Difference = " << res << endl;
+
+
+    return 0;
+}
